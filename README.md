1d43f2053231d089daa6ab36692a42daa440729d
Thomas van Dijk <tcdijk@gmail.com>
2020-06-11 21:24:56 +0200

    Code and readme

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# haelight
-Syntax highlighting for text layers in After Effects
+# hAElight
+Syntax highlighting for C++ code in After Effects text layers.
+
+I can't seem set colours at a character level in After Effects scripting, so the underlying technology is a bit ... unconventional. Also, hAElight does not properly parse the code, so you may get some funky results if you're unlucky.
+
+But usually it works.
+
+## Installation
+Download [haelight.jsx](haelight.jsx), put it somewhere you can find it, then use as described below. Optionally, "install" it in After Effects using `File > Scripts > Install Script File...`.
+
+## Usage
+Select one or more text layers in After Effects, then run `haelight.jsx` using:
+* `File > Scripts > Run Script File...`, or
+* `File > Scripts > haelight.jsx` if you have installed it.
+
+![Run Script File...](step1.png)
+
+![Result](step2.png)
+
+The script looks at the current contents of the text layer and stores the colours based on the _position_ in the text; the colours are not associated to the characters themselves. As a result, if you change the text of the layer, everything will become messed up and you should:
+1. Remove the `Haelight Setup` and `Haelight Apply` animators from the layer, and
+2. run hAElight again on it again.
+
+## Configuring the colours
+
+There is currently no UI to configure the colours; look at the top of `haelight.jsx` and change the `colors` list. Colours are given as three numbers `[red,green,blue]`, where each is between 0 and 1.
+
+## Under the hood
+
+I wasn't able to figure out how to set text colour on a character level from After Effects scripting. That would have been the sensible way to do this.
+
+Doing the actual syntax analysis "live" in After Effects animators and expressions turned out to be very slow and/or imprecise. (Fair enough.)
+
+So here's the ridiculous way to do it. We first figure out the colours we want. (Using regular expressions, so it's a bit fickle.)
+Then we generate an animator expression that assigns characters a colour based on their position (offset) in the string. Making a big array with the colour of each character results in a very large expression and was still very slow. But the colours don't change _that_ often, so we can make an expression that searches the (much shorter) list of changes given the `textIndex`. _Actually,_ we bake a binary search with nested if statements. For example, for
+
+    0123456789012345678901   <-- offset
+    return “foo”;//comment   <-- text
+
+we generate the following expression (pretty printed for readability).
+
+```javascript
+    var i = textIndex - 1;
+    if (i < 12)
+    {
+       if (i < 7)
+          [ 0.77, 0.53, 0.75 ];
+       else
+          [ 0.81, 0.57, 0.47 ];
+    }
+    else
+    {
+       if (i < 13)
+          [ 0.83, 0.83, 0.83 ];
+       else
+          [ 0.42, 0.60, 0.33 ];
+    }
+```
+
+Is this a weird way to do it? Yes. But here we are.
+
+If we want to make the expression smaller and/or faster:
+* To improve expression length, we could include a binary search function in the expression and have it search a list, rather than repeating all the ifs and elses. This would be a win for longer lists (but it needs to amortise the search code).
+* To improve expression length, we could factor out the color constants and go through a lookup table: an index into the table is smaller than the literal. This would also make it easier to tweak colours after the fact in the expression itself. This would be a win for longer lists (but it needs to amortise the overhead of including the table).
+* To improve the runtime of the expression, we could bias the search procedure using Huffman coding, so we get to large intervals more quickly. We _know_ the expression gets evaluated once for each `textIndex`...
---
+++ haelight.jsx
@@ -0,0 +1,155 @@
+﻿// haelight.jsx
+// Copyright 2020 Thomas van Dijk
+//
+// USAGE
+// Select one or more text layers, then run the script.
+//
+// Configure the colors below.
+var colors = {
+    plain:     {color:"[0.61,0.86,1]", prio:0 },
+    nonalpha:  {color:"[0.83,0.83,0.83]", prio:3 },
+    numbers:   {color:"[0.71,0.81,0.66]", prio:1 },
+    fun:       {color:"[0.86,0.86,0.67]", prio:1 },
+    keyword:   {color:"[0.77,0.53,0.75]", prio:2 },
+    in_angled: {color:"[0.81,0.57,0.47]", prio:3 },
+    type:      {color:"[0.34,0.61,0.84]", prio:3 },
+    comment:   {color:"[0.42,0.60,0.33]", prio:4 },
+    string:    {color:"[0.81,0.57,0.47]", prio:2 },
+}; 
+
+
+function cleanup_changes(text,changes,colors) {
+    changes.sort( function (x,y) { return x.at-y.at; } );
+    // clean up double occurrences
+    var out = []
+    var at = -1;
+    for( var i=0; i<changes.length; ++i ) {
+        var c = changes[i];
+        if( c.at>=text.length ) {
+            break;
+        } else if( c.at>at ) {
+            at = c.at;
+            out.push(c);
+            
+        } else if( colors[c.color].prio>colors[out[out.length-1].color].prio ) {
+            out[out.length-1].color = c.color;
+        }
+    }
+    // find the newlines
+    var newlines = []
+    var at = 0;
+    var limit = 1000000;
+    while ( (at=text.indexOf("\r",at)) != -1 && limit > 0) {
+        newlines.push(at);
+        at = at+1;
+        limit = limit-1;
+    }
+    // correct output indices for newlines
+    var newline_correction = 0;
+    for( var i=0; i<out.length; ++i ) {
+        while( newlines[newline_correction] < out[i].at && newline_correction<newlines.length ) {
+            newline_correction = newline_correction+1;
+        }
+        out[i].at = out[i].at - newline_correction;
+    }
+    return out;
+}
+function cst(list,colors,lo,hi) {
+    // Driven by compile_search_tree.
+    // Recursively generate AE expression search tree for the interval:
+    // list[lo].at inclusive, to list[hi+1].at exclusive
+    var dist = hi-lo;
+    if( dist==0 ) {
+        var hi_code = colors[list[lo].color].color;
+        return hi_code;
+    } else if( dist==1 ) {
+        var lo_code = colors[list[lo].color].color;
+        var hi_code = colors[list[hi].color].color;
+        return "if(i<"+list[hi].at+")"+lo_code+";else "+hi_code+";"
+    } else {
+        var mid = Math.floor((lo+hi)/2)
+        var lo_code = cst(list,colors,lo,mid);
+        var hi_code = cst(list,colors,mid+1,hi);
+        return "if(i<"+list[mid+1].at+"){"+lo_code+"}else{"+hi_code+"}";
+    }
+}
+function compile_search_tree(list,colors) {
+    // generate AE expression from change list
+    return cst(list,colors,0,list.length-1);
+}
+function mark(text,changes,color,regexp) {
+    // Add colour changes for all matches of regexp.
+    // Overrides all other changes in the matched intervals
+    // by moving their position to 1000000. This should probably
+    // be some kind of tree structure rather than going through
+    // the list every time.
+    while ((match = regexp.exec(text)) !== null) {
+        var start = match.index;
+        var end = regexp.lastIndex;
+        for( var i=0; i<changes.length; ++i ) {
+            if(start<changes[i].at&&changes[i].at<end) changes[i].at = 1000000;
+        }
+        changes.push({at:start,color:color});
+        changes.push({at:end,color:"plain"});
+    }
+}
+function highlight(layer) {
+    //
+    // === get some data and set up the animators
+    var text = layer.text.sourceText.value.toString();
+    // Set up base color
+    var animator_setup = layer.Text.Animators.addProperty("ADBE Text Animator");
+    animator_setup.name = "Haelight Setup";
+    var fillColor_setup = animator_setup.property("ADBE Text Animator Properties").addProperty("ADBE Text Fill Color");
+    fillColor_setup.setValue([0,0,0]);
+    // Animator for syntax colouring.
+    var animator = layer.Text.Animators.addProperty("ADBE Text Animator");
+    animator.name = "Haelight Apply";
+    var fillColor = animator.property("ADBE Text Animator Properties").addProperty("ADBE Text Fill Color");
+    fillColor.setValue([100,100,100]);
+    var selector = animator.property("ADBE Text Selectors").addProperty("ADBE Text Expressible Selector");
+    
+    // Start on "plain" colour at index 0.
+    changes = [{at:0,color:"plain"}];
+    // Then apply colours based on various regex
+    //mark(text,changes,"type",/(unsigned\w+)?(const\w+)?\**(void|char|int|float|double)(\w+const)?\**/g);
+    mark(text,changes,"numbers",/\d+/g);
+    mark(text,changes,"fun",/\w+\(/g);
+    mark(text,changes,"type",/(^|[\s(<>\(\)\{\}\[\]])(unsigned\w+)?(const\w+)?\**(void|char|int|float|double|bool|true|false)(\w+const)?\**($|[\s(<>\(\)\{\}\[\]:;])/g);
+    mark(text,changes,"keyword",/(^|[\s(<>\(\)\{\}\[\]])(extern|class|struct|template|typedef|public|protected|private|using|for|while|do|if|else|throw|catch|return|#include)($|[\s(<>\(\)\{\}\[\]:;])/g);
+    mark(text,changes,"type",/namespace/g);
+    mark(text,changes,"nonalpha",/[\(\)\[\]\{\}+\-*\/;:<>~]+/g);
+    mark(text,changes,"string",/[“"][^"”]*["”]/g);
+    mark(text,changes,"comment",/\/\/[^\r\n]*/g);
+    
+    // Turn the colour changes into an AE expression
+    var list = cleanup_changes(text,changes,colors);
+    var intro = "var i=textIndex-1;";
+    var search_tree = compile_search_tree(list,colors);
+    var compiled_expression = intro + search_tree;
+
+    // Apply it to the animator
+    selector.amount.expression = compiled_expression;
+}
+
+// Loop over the selected layers and process the text layers
+app.beginUndoGroup("Apply hAElight");
+var comp = app.project.activeItem;
+var selectedLayers = comp.selectedLayers;
+var did_something = false;
+if( selectedLayers!=null ) {
+    for( var i=0; i<comp.selectedLayers.length; ++i ) {
+        var layer = comp.selectedLayers[i];
+        if( layer.property("Source Text")!=null ) {
+            $.writeln("Applying hAElight to ["+layer.name +"].");
+            highlight(layer);
+            did_something = true;
+        } else {
+            $.writeln("["+layer.name +"] is a not text layer; skipping.");
+        }
+    }
+}
+if( !did_something ) {
+    alert( "No text layers selected. Did not do anything." );
+}
+app.endUndoGroup();
